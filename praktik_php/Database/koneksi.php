8dfa0fc4173ca17838a80129ffd8a6b966b07520
mashhisyam <haryoh48@gmail.com>
2020-12-08 15:49:22 +0700

    Ubah Carousel

--- praktik_php/Database/koneksi.php
+++ praktik_php/Database/koneksi.php
@@ -1,8 +1,8 @@
 <?php
     $namahost = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "praktikumdb";
+    $username = "id15622918_mashisyam";
+    $password = "Ztyj_\j{QftZNG9|";
+    $database = "id15622918_hisyamdb";
 
     $connect = mysqli_connect($namahost, $username, $password, $database);
 ?>
